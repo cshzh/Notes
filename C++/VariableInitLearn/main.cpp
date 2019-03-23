36046e61d11e9d9e3f31a6b937cfad2f7779ae1d
hui <hzh@mail.imu.edu.cn>
2019-03-23 21:47:27 +0800

    变量初始化的学习

---
+++ C++/VariableInitLearn/main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+class People {
+public:
+    int id;
+    char *name;
+
+    People(const char name[], int id) {
+        cout << "1 or 2 construct function call..." << endl;
+        this->name = new char[32];
+        strcpy(this->name, name);
+        this->id = id;
+    }
+
+    People() {
+        cout << "0 construct function call..." << endl;
+        this->name = new char[32];
+        this->id = -1;
+    }
+
+    People(const People &people) {
+        cout << "copy construct function call..." << endl;
+        this->name = new char[32];
+        strcpy(name, people.name);
+        this->id = people.id;
+    }
+
+    ~People() {
+//        cout << "destructor..." << endl;
+        delete[]name;
+    }
+
+    People &operator=(const People &people) {
+        cout << "operator=(people) call..." << endl;
+        if (this == &people) {
+            return *this;
+        }
+
+        delete[] this->name;
+        this->name = new char[strlen(people.name) + 1];
+        strcpy(this->name, people.name);
+        this->id = people.id;
+        return *this;
+    }
+
+    People &operator=(const char *const &name) {
+        cout << "operator=(name) call..." << endl;
+
+        delete[] this->name;
+        this->name = new char[strlen(name) + 1];
+        strcpy(this->name, name);
+        return *this;
+    }
+};
+
+void InitLearn();
+
+void show(const std::string &name, const People &people);
+
+int main() {
+    People people("hzh", 23);
+
+    show("people", people);
+
+    People people1;
+    people1 = "qwe";
+
+    show("people1", people1);
+
+//    InitLearn();
+
+}
+
+void show(const std::string &name, const People &people) {
+    std::cout << name << "  :" << std::endl;
+    std::cout << "id:{" << people.id << "}" << " " << "name:{" << people.name << "}" << std::endl;
+}
+
+void InitLearn() {
+    std::vector<int> v;
+
+    People people;
+    std::cout << "People  :" << std::endl;
+    std::cout << "id:{" << people.id << "}" << " " << "name:{" << people.name << "}" << std::endl;
+
+    People people1();
+    std::cout << "People2 :" << std::endl;
+//  std::cout << "id:{" << people2.id << "}" << " " << "name:{" << people2.name << "}" << std::endl;
+
+    People *people2;
+    people2 = new People();
+    std::cout << "People3 :" << std::endl;
+    std::cout << "id:{" << people2->id << "}" << " " << "name:{" << people2->name << "}" << std::endl;
+}
