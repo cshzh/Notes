084ed0afaae0d01984c098799baaa08613851846
hui <hzh@mail.imu.edu.cn>
2019-03-24 21:54:14 +0800

    add network io

--- NIO/IOLearn/main.cpp
+++ NIO/IOLearn/main.cpp
@@ -5,9 +5,30 @@
 #include <fcntl.h>
 #include <cerrno>
 #include <cstring>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+void ReadFile();
 
 int main() {
+    const char *src = "128.2.194.242";
+    int *dst = new int{};
+    printf("%s\n", get_current_dir_name());
 
+    if (inet_pton(AF_INET, src, dst) > 0) {
+        printf("%#x\n", *dst);
+        printf("%#x\n", htonl(*dst));
+        printf("%#x\n", htonl(htonl(*dst)));
+    } else {
+        printf("%s\n", strerror(errno));
+    }
+
+//    ReadFile();
+
+    return 0;
+}
+
+void ReadFile() {
     int fd = open("/home/hui/Projects/Notes/NIO/IOLearn/CMakeLists.txt", O_RDONLY, 0);
 
     if (fd < 0) {
@@ -21,6 +42,4 @@ int main() {
 
         close(fd);
     }
-
-    return 0;
 }
