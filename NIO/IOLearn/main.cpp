fe51e74b7550f6e1394a7a857465049558657ead
hui <hzh@mail.imu.edu.cn>
2019-03-24 20:48:41 +0800

    read CMakeLists.txt file

--- NIO/IOLearn/main.cpp
+++ NIO/IOLearn/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <cerrno>
+#include <cstring>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    int fd = open("/home/hui/Projects/Notes/NIO/IOLearn/CMakeLists.txt", O_RDONLY, 0);
+
+    if (fd < 0) {
+        printf("open file fail.\n");
+        printf("%s\n", strerror(fd));
+    } else {
+        char buf[1024] = {};
+
+        long length = read(fd, buf, 1024);
+        printf("%s\n", buf);
+
+        close(fd);
+    }
+
     return 0;
 }
