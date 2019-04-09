01a8a341f0b465045ae8dca3f0a22c0603f9602d
hui <hzh@mail.imu.edu.cn>
2019-04-09 20:46:38 +0800

    add csapp source code

--- C++/SignalLearn/main.cpp
+++ C++/SignalLearn/main.cpp
@@ -2,21 +2,15 @@
 #include <cstring>
 #include <cerrno>
 
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-
-
-int Fork();
-
-sighandler_t Signal(int signum, sighandler_t handler);
+extern "C" {
+#include "csapp.h"
+}
 
 void Sighandler(int signum) {
     while (waitpid(-1, nullptr, WNOHANG) > 0) {
         printf("Handler reaped child\n");
     }
 }
-
 
 int main() {
     pid_t pid = -1;
@@ -41,25 +35,4 @@ int main() {
     }
 
     return 0;
-}
-
-sighandler_t Signal(int signum, sighandler_t handler) {
-    sighandler_t sighandler;
-
-    if ((sighandler = signal(signum, handler)) == SIG_ERR) {
-        fprintf(stderr, "signal error: %s\n", strerror(errno));
-        _exit(1);
-    }
-
-    return sighandler;
-}
-
-int Fork() {
-    pid_t pid;
-
-    if ((pid = fork()) < 0) {
-        fprintf(stderr, "fork error: %s\n", strerror(errno));
-        _exit(1);
-    }
-    return pid;
 }
