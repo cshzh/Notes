ac5507281c07d7d2d4a9be16afda75e4b536f59b
hui <hzh@mail.imu.edu.cn>
2019-06-03 15:36:37 +0800

    init N-Queens

---
+++ Algorithm/NQueens/Solution.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hui on 19-6-3.
+//
+
+#include "Solution.h"
---
+++ Algorithm/NQueens/Solution.h
@@ -0,0 +1,20 @@
+//
+// Created by hui on 19-6-3.
+//
+
+#ifndef NQUEENS_SOLUTION_H
+#define NQUEENS_SOLUTION_H
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+
+    }
+};
+
+
+#endif //NQUEENS_SOLUTION_H
