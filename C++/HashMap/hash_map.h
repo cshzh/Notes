a437950a2dd4d9d12f767733dff57c7c0a922728
huang zhaohui <hzh@mail.imu.edu.cn>
2019-06-25 09:03:20 +0800

    1. 完成hash_map测试用例

---
+++ C++/HashMap/hash_map.h
@@ -0,0 +1,38 @@
+//
+// Created by hui on 19-6-24.
+//
+
+#ifndef HASHMAP_HASH_MAP_H
+#define HASHMAP_HASH_MAP_H
+
+class hash_map {
+public:
+    hash_map() {
+    }
+
+    void put(int key, int value);
+
+    int get(int key);
+
+    void erase(int key);
+
+    int size();
+};
+
+void hash_map::put(int key, int value) {
+
+}
+
+int hash_map::get(int key) {
+    return 0;
+}
+
+int hash_map::size() {
+    return 0;
+}
+
+void hash_map::erase(int key) {
+
+}
+
+#endif //HASHMAP_HASH_MAP_H
---
+++ C++/HashMap/hash_map_test.cpp
@@ -0,0 +1,84 @@
+//
+// Created by hui on 19-6-24.
+//
+
+#include "gtest/gtest.h"
+#include "hash_map.h"
+
+TEST(hash_map_test, put) {
+    hash_map map;
+
+    map.put(1, 1);
+    EXPECT_EQ(1, map.size());
+    map.put(1, 2);
+    EXPECT_EQ(1, map.size());
+    map.put(2, 2);
+    EXPECT_EQ(2, map.size());
+    map.put(3, 2);
+    EXPECT_EQ(3, map.size());
+    map.put(3, 3);
+    EXPECT_EQ(3, map.size());
+}
+
+TEST(hash_map_test, get) {
+    hash_map map;
+
+    map.put(10, 10);
+    EXPECT_EQ(10, map.get(10));
+    map.put(8, 8);
+    EXPECT_EQ(8, map.get(8));
+    map.put(6, 6);
+    EXPECT_EQ(6, map.get(6));
+    map.put(4, 4);
+    EXPECT_EQ(4, map.get(4));
+    map.put(2, 2);
+    EXPECT_EQ(2, map.get(2));
+    map.put(0, 0);
+    EXPECT_EQ(0, map.get(0));
+}
+
+TEST(hash_map_test, erase) {
+    hash_map map;
+    map.put(1, 1);
+    map.put(2, 2);
+    map.erase(1);
+    EXPECT_EQ(1, map.size());
+    map.put(3, 3);
+    map.erase(2);
+    EXPECT_EQ(1, map.size());
+    map.put(4, 4);
+    map.erase(3);
+    EXPECT_EQ(1, map.size());
+    map.put(5, 5);
+    map.put(6, 6);
+    map.put(7, 7);
+    map.erase(5);
+    EXPECT_EQ(3, map.size());
+    map.erase(4);
+    map.erase(6);
+    map.erase(7);
+    EXPECT_EQ(0, map.size());
+}
+
+TEST(hash_map_test, integration) {
+    hash_map map;
+
+    for (int i = 1; i < 1000; i += 2) {
+        map.put(i, i + 1);
+        EXPECT_EQ(i + 1, map.get(i));
+        EXPECT_EQ((i + 1) / 2, map.size());
+    }
+
+    EXPECT_EQ(500, map.size());
+
+    EXPECT_EQ(2, map.get(1));
+    EXPECT_EQ(8, map.get(7));
+    EXPECT_EQ(1000, map.get(999));
+
+    for (int i = 999; i > 0; i -= 2) {
+        EXPECT_EQ(i + 1, map.get(i));
+        EXPECT_EQ((i + 1) / 2, map.size());
+        map.erase(i);
+    }
+}
+
